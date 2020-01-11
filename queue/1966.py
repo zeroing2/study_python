533572088a3b304e3009e2a49d8406a63bdea0cc
zeroing2 <zeroing2@gmail.com>
2020-01-11 15:08:47 +0900

    [changed_minor] solve the problem

--- queue/1966.py
+++ queue/1966.py
@@ -1,69 +1,68 @@
-import sys
-from collections import deque
+class Node:
+    def __init__(self, priority, first_index):
+        self.priority = priority
+        self.first_index = first_index
 
-queue = deque()
+    def get_priority(self):
+        return self.priority
 
-
-def push(x):
-    global queue
-    queue.append(x)
-
-
-def pop():
-    global queue
-    if len(queue) > 0:
-        print(queue.popleft())
-    else:
-        print(-1)
+    def get_first_index(self):
+        return self.first_index
 
 
-def size():
-    global queue
-    print(len(queue))
-
-
-def empty():
-    global queue
-    if len(queue) > 0:
-        print(0)
-    else:
-        print(1)
-
-
-def front():
-    global queue
-    if len(queue) > 0:
-        print(queue[0])
-    else:
-        print(-1)
-
-
-def back():
-    global queue
-    if len(queue) > 0:
-        print(queue[-1])
-    else:
-        print(-1)
-
-
-def execute_command(command):
-    if 'push' in command:
-        push(int(command[1]))
-    elif 'pop' in command:
-        pop()
-    elif 'front' in command:
-        front()
-    elif 'back' in command:
-        back()
-    elif 'empty' in command:
-        empty()
-    elif 'size' in command:
-        size()
-    else:
-        print("invalid command")
-
+q = list()
 
 iteration_num = int(input())
 for i in range(iteration_num):
-    command = sys.stdin.readline().split()
-    execute_command(command)
+    cmd = list(map(int, input().split()))
+    priority = list(map(int, input().split()))
+    target_index = cmd[1]
+    # print(cmd)
+    # print(priority)
+    for j in range(len(priority)):
+        node = Node(priority[j], j)
+        q.append(node)
+
+    cnt = 0
+    while len(q) > 0:
+        greater = False
+        equal = False
+        pop_priority = q[0].get_priority()
+        pop_index = q[0].get_first_index()
+        del q[0]
+
+        for j in range(len(q)):
+            if pop_priority < q[j].get_priority():
+                greater = True
+            elif pop_priority == q[j].get_priority():
+                equal = True
+
+        # print("[" + str(pop_priority) + "], ", end='')
+        # print("[", end="")
+        # for j in range(len(q)):
+        #     print(str(q[j].get_priority()) + ",", end='')
+        # print("]")
+        #
+        # print("pop_index = %d, target_index = %d, queue_size: %d" % (pop_index, target_index, len(q)))
+        #
+        # print("greater = ", greater)
+        # print("equal = ", equal)
+
+        if target_index == pop_index:
+            if greater:
+                node = Node(pop_priority, pop_index)
+                q.append(node)
+            else:
+                cnt = cnt + 1
+                break
+        else:
+            if greater:
+                node = Node(pop_priority, pop_index)
+                q.append(node)
+            else:
+                cnt = cnt + 1
+
+    print(cnt)
+    # print("que len: ", len(q))
+    q.clear()
+    # if()
